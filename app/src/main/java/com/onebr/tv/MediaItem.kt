0a759eb29df8121efe90d1ecebae7adade4f5a79
baqer2007 <168880151+baqer2007@users.noreply.github.com>
2026-09-01 22:14:24 +0300

    Update MediaItem.kt

--- app/src/main/java/com/onebr/tv/MediaItem.kt
+++ app/src/main/java/com/onebr/tv/MediaItem.kt
@@ -3,16 +3,13 @@ package com.onebr.tv
 import com.google.gson.annotations.SerializedName
 
 data class MediaResponse(
-    @SerializedName("results") val results: List<MediaItem>?
+    @SerializedName("results") val results: List<MediaItem>? = null
 )
 
 data class MediaItem(
-    @SerializedName("id") val id: Int,
-    @SerializedName("title") val title: String?,
-    @SerializedName("name") val name: String?,
-    @SerializedName("poster_path") val poster_path: String?,
-    @SerializedName("backdrop_path") val backdrop_path: String?,
-    @SerializedName("overview") val overview: String?,
-    @SerializedName("vote_average") val vote_average: Double?,
-    @SerializedName("media_type") val media_type: String?
+    @SerializedName("id") val id: Int = 0,
+    @SerializedName("title") val title: String? = null,
+    @SerializedName("name") val name: String? = null,
+    @SerializedName("poster_path") val poster_path: String? = null,
+    @SerializedName("media_type") val media_type: String? = null
 )
